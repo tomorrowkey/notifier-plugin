e64ef8cae5835b52426dc719ce0481245a79c0b4
Tomoki YAMASHITA <tomorrowkey@gmail.com>
2015-05-09 16:28:19 +0900

    Support java8 (Do not use ConfigSlurper anymore)

--- plugin/src/main/groovy/jp/tomorrowkey/gradle/notifier/NotifiersFactory.groovy
+++ plugin/src/main/groovy/jp/tomorrowkey/gradle/notifier/NotifiersFactory.groovy
@@ -4,49 +4,50 @@ import org.gradle.api.Project
 
 public class NotifiersFactory {
 
-    private static final String DEFAULT_CONFIG_FILE_PATH = "default.groovy";
-
-    private static final String CONFIG_FILE_PATH = "notifier.groovy";
+    private static final String CONFIG_FILE_PATH = "notifier.properties";
 
     public static Notifier[] create(Project project) {
-        Map config = getDefaultConfig().merge(getConfig(CONFIG_FILE_PATH));
-        return create(project, config)
+        Map config = getDefaultConfig()
+        config.putAll(getConfig(new File(CONFIG_FILE_PATH)))
+        create(project, config)
     }
 
-    static ConfigObject getDefaultConfig() {
-        return new ConfigSlurper().parse(Thread.currentThread().getContextClassLoader().getResource(DEFAULT_CONFIG_FILE_PATH));
+    static Map getDefaultConfig() {
+        def Map map = new HashMap()
+        map.put('voice.enabled', false)
+        map.put('voice.name', 'Alex')
+        map.put('notificationCenter.enabled', false)
+        map.put('sound.enabled', false)
+        map.put('beep.enabled', false)
+        map.put('beep.count', 3)
+        return map
     }
 
-    static ConfigObject getConfig(String configFilePath) {
-        def file = new File(configFilePath);
-        if (file.exists()) {
-            return new ConfigSlurper().parse(file.toURI().toURL());
-        } else {
-            return new ConfigObject();
+    static Map getConfig(File configFile) {
+        def props = new Properties();
+        configFile.withInputStream {
+            stream -> props.load(stream)
         }
+        return props
     }
 
     static Notifier[] create(Project project, Map config) {
         def notifiers = []
 
-        if (config.voice.enabled) {
-            notifiers.add(new VoiceNotifier(config.voice.name))
+        if (config.get('voice.enabled').toBoolean()) {
+            notifiers.add(new VoiceNotifier(config.get('voice.name')))
         }
 
-        if (config.notificationCenter.enabled) {
+        if (config.get('notificationCenter.enabled').toBoolean()) {
             notifiers.add(new NotificationCenterNotifier())
         }
 
-        if (config.sound.enabled) {
-            notifiers.add(new SoundNotifier(project, config.sound.url))
+        if (config.get('sound.enabled').toBoolean()) {
+            notifiers.add(new SoundNotifier(project, config.get('sound.url')))
         }
 
-        if (config.beep.enabled) {
-            if (config.beep.count instanceof Integer) {
-                notifiers.add(new BeepNotifier(config.beep.count));
-            } else {
-                notifiers.add(new BeepNotifier());
-            }
+        if (config.get('beep.enabled').toBoolean()) {
+            notifiers.add(new BeepNotifier(config.get('beep.count').toInteger()));
         }
 
         return notifiers;
